874d718bf811c93b683c5027e85265862021f422
chenpan <13396073613@163.com>
2016-11-25 16:35:22 +0800

    11.25

--- WebBase/WebContent/JavaScript/ScriptDemo.js
+++ WebBase/WebContent/JavaScript/ScriptDemo.js
@@ -1,6 +1,24 @@
 /**
  *
  */
-alert("nice to meet you");
+function demo() {
+	document.write("nice to meet you");
+}
+function demo1() {
 	
+	var len1 =navigator.appCodeName;
+	var len2 =navigator.appName;
+	var len3=navigator.appVersion;
+	var len4 = navigator.plugins.length;
+	with(document){
+		
+		write(len1+"<br/>");
+		write(len2+"<br/>");
+		write(len3+"<br/>");
+		write(len4);
+	}
 	
+}
+function demo3() {
+	var window1 = windows.name;
+}
